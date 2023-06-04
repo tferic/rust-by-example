f7d6bc4e16c862bb33d0d88838378793262fa8ae
Toni Feric <toni.feric@belsoft.ch>
2023-06-04 12:17:40 +0200

    Finish "Testcase: List"

--- 01.hello.world/formatted-print-display/src/main.rs
+++ 01.hello.world/formatted-print-display/src/main.rs
@@ -43,6 +43,31 @@ impl fmt::Display for Complex {
     }
 }
 
+// Define a structure named `List` containing a `Vec`.
+struct List(Vec<i32>);
+
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Extract the value using tuple indexing,
+        // and create a reference to `vec`.
+        let vec = &self.0;
+
+        write!(f, "[")?;
+
+        // Iterate over `v` in `vec` while enumerating the iteration
+        // count in `count`.
+        for (count, v) in vec.iter().enumerate() {
+            // For every element except the first, add a comma.
+            // Use the ? operator to return on errors.
+            if count != 0 { write!(f, ", ")?; }
+            write!(f, "{}: {}", count, v)?;
+        }
+
+        // Close the opened bracket and return a fmt::Result value.
+        write!(f, "]")
+    }
+}
+
 fn main() {
     let minmax = MinMax(0, 14);
 
@@ -72,4 +97,7 @@ fn main() {
     println!("Complex number assignment:");
     println!("Display: {}", complex_point1);
     println!("Debug: {:?}", complex_point1);
+
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
 }
